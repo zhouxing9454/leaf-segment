d6dd0e025ccd3c620b4b6798070782c583127d4e
纸鸢 <76584599+zhouxing9454@users.noreply.github.com>
2024-11-07 11:31:29 +0800

    Update data.go

--- core/data.go
+++ core/data.go
@@ -8,6 +8,21 @@ import (
 	"time"
 )
 
+/*
+	create database leaf-segment;
+	
+	CREATE TABLE `segments` (
+	 `biz_tag` varchar(32) NOT NULL,
+	 `max_id` bigint NOT NULL,
+	 `step` bigint NOT NULL,
+	 `description` varchar(1024) DEFAULT '' NOT NULL,
+	 `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	 PRIMARY KEY (`biz_tag`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	
+	INSERT INTO segments(`biz_tag`, `max_id`, `step`, `description`) VALUES('test', 0, 100000, "test业务");
+*/
+
 type Data struct {
 	db *sql.DB // 数据库连接对象
 }
